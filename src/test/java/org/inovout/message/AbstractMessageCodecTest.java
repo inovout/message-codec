aabc279c553ee4c17d4b9d1030fe41b35d7b64fe
huay <huay@njcky.com>
2021-07-07 17:25:14 +0800

    支持Boolean、Map和Array类型

--- src/test/java/org/inovout/message/AbstractMessageCodecTest.java
+++ src/test/java/org/inovout/message/AbstractMessageCodecTest.java
@@ -1,7 +1,5 @@
 package org.inovout.message;
 
-import org.inovout.message.data.MessagePacketData;
-
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -14,13 +12,21 @@ public abstract class AbstractMessageCodecTest {
         assertEquals(expected.toUpperCase(), actual.toUpperCase());
     }
 
-    protected final void asserDecodeResult(Map<String, Object> data, MessagePacketData packetData) {
+    protected final void asserDecodeResult(Map<String, Object> data, Map<String, Object> packetData) {
         for (String name : data.keySet()) {
             Object expected = data.get(name);
             Object actual = packetData.get(name);
-            if (expected instanceof byte[]) {
+            if (byte[].class.equals(expected.getClass())) {
                 assertArrayEquals((byte[]) expected, (byte[]) actual, name);
 
+            } else if (Short[].class.equals(expected.getClass())) {
+                assertArrayEquals((Short[]) expected, (Short[]) actual, name);
+            } else if (Integer[].class.equals(expected.getClass())) {
+                assertArrayEquals((Integer[]) expected, (Integer[]) actual, name);
+            } else if (Long[].class.equals(expected.getClass())) {
+                assertArrayEquals((Long[]) expected, (Long[]) actual, name);
+            } else if (expected instanceof Map) {
+                asserDecodeResult((Map<String, Object>) expected, (Map<String, Object>) actual);
             } else {
                 assertEquals(expected, actual, name);
             }
